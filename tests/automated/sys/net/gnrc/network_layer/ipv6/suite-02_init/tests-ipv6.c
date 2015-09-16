6fcb7da2fd6042998b18ec66ba37f032d4c66916
Andreas Paul Pauli <andreas.pauli@haw-hamburg.de>
2015-09-17 01:48:42 +0200

    suite-02_init: implemented scheduler pool flooding, adapted comments

--- tests/automated/sys/net/gnrc/network_layer/ipv6/suite-02_init/tests-ipv6.c
+++ tests/automated/sys/net/gnrc/network_layer/ipv6/suite-02_init/tests-ipv6.c
@@ -19,49 +19,65 @@
 
 #include "kernel_types.h"
 #include "byteorder.h"
+#include "thread.h"
 
 #include "net/gnrc/ipv6.h"
 
 #include "../tests-ipv6.h"
 
+/*
+ * Function representing threads, spawned in set_up().
+ */
+static void *thread_func(void *arg)
+{
+    return arg;
+}
 
 static void set_up(void)
 {
-    ;
+    /* Guarding the loop count */
+    unsigned thr_count = 0;
+    /* Shared stack is only sufficient for non running threads */
+    char dummy_stack[THREAD_STACKSIZE_DEFAULT];
+    kernel_pid_t thr_id = KERNEL_PID_UNDEF;
+
+    puts("Start spawning");
+    do {
+        thr_id = thread_create(
+                     dummy_stack, sizeof(dummy_stack),
+                     THREAD_PRIORITY_MAIN - 1,
+                     CREATE_SLEEPING | CREATE_STACKTEST,
+                     thread_func, NULL, "dummy");
+        ++thr_count;
+    }
+    while ((-EOVERFLOW != thr_id) && (MAXTHREADS >= thr_count));
+    puts("Finished spawning");
 }
-
-
-static void tear_down(void)
-{
-    ;
-}
-
 
 /*
  * Tests for function "gnrc_ipv6_init"
  */
 
 /**
- * Tests check if a free pid is available for module.
- * Deferred!
+ * Tests ipv6 module thread initialisation.
+ * Relying on successfull test of performed initialisation and prevention
+ * of second thread creation.
  *
- * @pre: No IPV6 thread has been initialized and MAXTHREADS threads already
- *       exist.
- * @post: No IPV6 thread has been initialized.
+ * @pre: No IPV6 thread has been initialized and MAXTHREADS threads
+ *       already exist.
+ * @post: Function has returned expected error value, and no IPV6 thread
+ *        has been initialized.
  *
- * @todo
- *   - Controlled creation of precondition.
  */
 static void test_ipv6_init__maxthr(void)
 {
-    const char *errstr = "!Test insufficient.!";
+    const char *errstr = "Thread creation not prevented.!";
     kernel_pid_t retval = KERNEL_PID_UNDEF;
 
     retval = gnrc_ipv6_init();
 
     TEST_ASSERT_MESSAGE(-EOVERFLOW == retval, errstr);
 }
-
 
 Test *ipv6_tests(void)
 {
