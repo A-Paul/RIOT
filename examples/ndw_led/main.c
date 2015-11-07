69583ad2822f4098a52b503f183805444058ef8e
Andreas Paul Pauli <andreas.pauli@haw-hamburg.de>
2015-11-07 10:31:13 +0100

    phytec driver is incapeable of low pwm frequencies

--- examples/ndw_led/main.c
+++ examples/ndw_led/main.c
@@ -13,8 +13,6 @@
  * @file
  * @brief       Test for low-level PWM drivers
  *
- *
- *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  *
  * @}
@@ -33,51 +31,47 @@
 #define CHANNELS    PWM_0_CHANNELS
 #define MODE        PWM_LEFT
 
-#define FREQU       (2U)
+#define FREQU       (20U)
 #define STEPS       (3072U)
 #define STEP        (STEPS/16)
 #define OFFS        (STEPS/3)
 
 int main(void)
 {
-  int ratio = 0;
-  int res   = 0;
-  int state = 0;
-  int step  = STEP;
+    int ratio = 0;
+    int res   = 0;
+    int state = 0;
+    int step  = STEP;
 
-  puts("\nRIOT PWM test");
-  puts("Connect an LED or scope to PWM pins to see something");
+    puts("\nRIOT PWM test");
+    puts("Connect an LED or scope to PWM pins to see something");
 
-  res = pwm_init(DEV, MODE, FREQU, STEPS);
-
-  if (res < 0) {
-
-
-    puts("Errors while initializing PWM");
-    return -1;
-  }
-
-  puts("PWM initialized.");
-  printf("requested: %d Hz, got %d Hz\n", FREQU, res);
-
-  while (1) {
-
-    puts("{R,G,B}:");
-    for (int i = 0; i < CHANNELS; i++) {
-      ratio = (state + i * OFFS) % STEPS;
-      pwm_set(DEV, i, ratio);
-      printf("|%04d", ratio);
-    }
-    puts("|");
-
-    state += step;
-    if (state < STEP || state > (STEPS - STEP)) {
-      step  = -step;
-      state += step;
+    res = pwm_init(DEV, MODE, FREQU, STEPS);
+    if (res < 0) {
+        puts("Errors while initializing PWM");
+        return 1;
     }
 
-    xtimer_usleep(WAIT);
-  }
+    puts("PWM initialized.");
+    printf("requested: %d Hz, got %d Hz\n", FREQU, res);
 
-  return 0;
+    while (1) {
+        puts("{R,G,B}:");
+        for (int i = 0; i < CHANNELS; i++) {
+            ratio = (state + i * OFFS) % STEPS;
+            pwm_set(DEV, i, ratio);
+            printf("|%04d", ratio);
+        }
+        puts("|");
+
+        state += step;
+        if (state < STEP || state > (STEPS - STEP)) {
+            step  = -step;
+            state += step;
+        }
+
+        xtimer_usleep(WAIT);
+    }
+
+    return 0;
 }
